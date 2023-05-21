de42ba151017a5db8ee6ca9c076effff3df7bc22
nabilgouri <38146132+nabilgouri@users.noreply.github.com>
2023-05-21 14:27:26 +0100

    added home routing

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ const Joi = require('joi');
 const morgan = require('morgan');
 const startupDebugger = require('debug')('app:startup');
 const genresRouter = require('./routes/genres');
+const homeRouter = require('./routes/home');
 
 const app = express();
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(express.json());
 app.use('/api/genres', genresRouter);
+app.use('/', homeRouter);
 
 if (app.get('env') === 'development') {
   startupDebugger('Morgan enabled ....');
   app.use(morgan('tiny'));
 }
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Vidly', message: 'Hello' });
-});
+
 const portNumber = process.env.PORT || 8000;
 
 app.listen(portNumber, () => console.log(`Listening on port ${portNumber}`));
